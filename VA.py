74628bba616f8ec8657f3ae357761ecd4ba3d694
Lizardguy2003 <acreider2003@gmail.com>
2025-05-20 22:41:24 +0200

    wtf

--- VA.py
+++ VA.py
@@ -2,14 +2,16 @@ from tkinter import *
 from tkinter import messagebox
 from random import *
 import math
+import numpy
 
 
 
 after_id = None
-
+balls = []
+animation_running = False
 
 root = Tk()
-root.title("Chicken and Balls")
+root.title("Balls")
 root.geometry('1000x1000')
 
 
@@ -20,7 +22,7 @@ control_frame.pack(side=TOP, pady=10)
 canvas = Canvas(control_frame, width = 500, height = 500, bg= 'blue')
 canvas.pack()
 
-Label(control_frame, text='speed').pack()
+Label(control_frame, text='Starting speed of balls').pack()
 speed = Entry(control_frame, width=30, bg="lightgray")
 speed.pack()
 
@@ -28,11 +30,9 @@ Label(control_frame, text='balls').pack()
 n_balls = Entry(control_frame, width=30, bg="lightgray")
 n_balls.pack()
 
-
-
-
-balls = []
-animation_running = False
+Label(control_frame, text='FPS').pack()
+fps = Entry(control_frame, width=30, bg="lightgray")
+fps.pack()
 
 def stopping():
     global balls, animation_running, after_id
@@ -46,7 +46,7 @@ def stopping():
 
 def starting():
     BALL_SIZE = 30
-    global balls, animation_running, after_id, speed_true
+    global balls, animation_running, after_id, speed_true, fps_true, vel
     balls.clear()
     canvas.delete('balls')
 
@@ -57,6 +57,7 @@ def starting():
     try:
         balls_true = int(n_balls.get())
         speed_true = int(speed.get())
+        fps_true = int(fps.get())
         for i in range(balls_true):
             x1 = randint(0, 500 - BALL_SIZE)
             y1 = randint(0, 500 - BALL_SIZE)
@@ -64,6 +65,8 @@ def starting():
             y2 = y1 + BALL_SIZE
             velx = choice([-1, 1]) * speed_true
             vely = choice([-1, 1]) * speed_true
+            vel = numpy.array([velx, vely])
+            
             ball_id = canvas.create_oval(x1, y1, x2, y2, fill="red", tags='balls')
             balls.append((ball_id, velx,vely))
     except ValueError:
@@ -81,21 +84,63 @@ def move_balls():
     for i in range(len(balls)):
         ball_id, velx, vely = balls[i]
         coords = canvas.coords(ball_id)
-        x1, y1, x2, y2 = coords
+        print(balls[i])
+        
 
         
-        if x1 + velx < 0 or x2 + velx > 500:
-            velx = choice([-1, 1]) * randint(1, speed_true)
-        if y1 +vely < 0 or y2 + vely > 500:
-            vely = choice([-1, 1]) * randint(1, speed_true)
+        if coords[0] + velx < 0 or coords[2] + velx > 500:
+            velx = choice([-1, 1]) * speed_true
+        if coords[1] +vely < 0 or coords[3] + vely > 500:
+            vely = choice([-1, 1]) * speed_true
 
         canvas.move(ball_id, velx,vely)
         balls[i] = (ball_id, velx,vely)
 
-    after_id = canvas.after(20, move_balls)
+
+        ball1_coords = canvas.coords(ball_id)
+
+        # Check collisions with other balls
+        for j in range(i + 1, len(balls)):
+            ball2_id, velx2, vely2 = balls[j]
+            ball2_coords = canvas.coords(ball2_id)
+            
+            coordx1 = (ball1_coords[0] + ball1_coords[2]) / 2
+            coordy1 = (ball1_coords[1] + ball1_coords[3]) / 2
+            coordx2 = (ball2_coords[0] + ball2_coords[2]) / 2
+            coordy2 = (ball2_coords[1] + ball2_coords[3]) / 2
+
+            coord1 = numpy.array([coordx1, coordy1])
+            coord2 = numpy.array([coordx2, coordy2])
+            if balls_touch(ball1_coords, ball2_coords):
+                
+
+                #calculate velocities
+                #vel1 = numpy.array([velx, vely])
+                vel2 = numpy.array([velx2, vely2])
+                
+                vel_new1 = vel + (numpy.dot((vel2-vel),(coord2 - coord1))/(numpy.abs(coord2-coord1)**2))*(coord2 - coord1)
+                vel_new2 = vel2 + (numpy.dot((vel-vel2),(coord1-coord2))/(numpy.abs(coord1-coord2)**2))*(coord1-coord2)
+                
+                balls[i] = (ball_id, vel_new1[0], vel_new1[1])
+                
+                balls[j] = (ball2_id, vel_new2[0], vel_new2[1])
+
+    after_id = canvas.after(int(1000/fps_true), move_balls)
     
     
+def balls_touch(ball1_coords, ball2_coords):
     
+    # center of balls coordinates
+    x1 = (ball1_coords[0] + ball1_coords[2]) / 2
+    y1 = (ball1_coords[1] + ball1_coords[3]) / 2
+    x2 = (ball2_coords[0] + ball2_coords[2]) / 2
+    y2 = (ball2_coords[1] + ball2_coords[3]) / 2
+
+    if math.sqrt((x2 - x1)**2 + (y2 - y1)**2) < 30:
+        return True
+    else:
+        return False
+
 
 
 start = Button(control_frame, text='start', command=starting)
